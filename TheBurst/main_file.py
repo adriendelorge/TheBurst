163df12636e2f449ed9b1088b3f897408cc1472a
Adrien Delorge <adriendelorge@libertysurf.fr>
2022-06-08 12:01:06 +0800

    new branch

--- TheBurst/main_file.py
+++ TheBurst/main_file.py
@@ -1,12 +1,18 @@
 from TheBurst.cities_data import get_cities_data
 from TheBurst.transports import get_airport_data, get_train_data
 from TheBurst.map import create_map
+from TheBurst.price_sqmeter import sqmeterprice_data
+from TheBurst.subsidies import subsidies
 
 #Merge Dataframes
 def final_dataframe():
     df=get_cities_data()
     df=df.merge(get_airport_data(),on='code_commune_INSEE',how='left')
     df=df.merge(get_train_data(),on='code_commune_INSEE',how='left')
+    df=df.merge(sqmeterprice_data(),on='code_commune_INSEE',how='left')
+    df=df.merge(subsidies(),on='code_commune_INSEE',how='left')
     return df
 
 #Queries
+def get_three_best_cities():
+    pass
